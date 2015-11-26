9273c52a8e5e0847603c136b1dad541e28222851
Viktor Borisov <viktor.borisov@bars-open.ru>
2015-11-26 16:05:48 +0600

    [*] Необходимые изменения для проекта WebSvody

--- src/Quartz/SystemTime.cs
+++ src/Quartz/SystemTime.cs
@@ -9,14 +9,42 @@ namespace Quartz
     /// </summary>
     public static class SystemTime
     {
+        private static object locker = new object();
+        private static TimeSpan timeDifference = new TimeSpan(0, 0, 0);
+
+        /// <summary>
+        /// –азница во времени
+        /// –азница между временем текущей системы и временем поставщика точного времени
+        /// </summary>
+        public static TimeSpan TimeDifference
+        {
+            get
+            {
+                return timeDifference;
+            }
+        }
+
         /// <summary>
         /// Return current UTC time via <see cref="Func&lt;T&gt;" />. Allows easier unit testing.
         /// </summary>
-        public static Func<DateTimeOffset> UtcNow = () => DateTimeOffset.UtcNow;
+        public static Func<DateTimeOffset> UtcNow = () => DateTimeOffset.UtcNow + SystemTime.TimeDifference;
 
         /// <summary>
         /// Return current time in current time zone via <see cref="Func&lt;T&gt;" />. Allows easier unit testing.
         /// </summary>
-        public static Func<DateTimeOffset> Now = () => DateTimeOffset.Now;
+        public static Func<DateTimeOffset> Now = () => DateTimeOffset.Now + SystemTime.TimeDifference;
+
+        /// <summary>
+        /// ћетод установки разницы во времени
+        /// ѕримен€етс€ дл€ установки разницы между временем текущей системы и временем поставщика точного времени
+        /// </summary>
+        /// <param name="timeDiff"></param>
+        public static void SetTimeDifference(TimeSpan timeDiff)
+        {
+            lock (locker)
+            {
+                timeDifference = timeDiff;
+            }
+        }
     }
 }
